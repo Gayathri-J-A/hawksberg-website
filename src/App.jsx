876cefd082d728871c4126f7b4866db0d7be5ae8
Gayathri J.A <jayayathri722@gmail.com>
2026-08-06 14:26:45 +0530

    Add dedicated ISO 27001 service page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import ServiceDetail from "./pages/ServiceDetail.jsx";
+import ISO27001 from "./pages/services/ISO27001.jsx";
 import TrainingDetail from "./pages/TrainingDetail.jsx";
 import IsoTrainingDetail from "./pages/IsoTrainingDetail.jsx";
 import InfoPageDetail from "./pages/InfoPageDetail.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import CourseDetail from "./pages/CourseDetail.jsx";
+import TrainingPortal from "./pages/TrainingPortal.jsx";
+import TrainingLogin from "@/pages/TrainingLogin.jsx";
+import AdminTraining from "@/pages/AdminTraining.jsx";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -26,12 +30,17 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services/iso-27001" element={<ISO27001 />}/>
         <Route path="/services/:slug" element={<ServiceDetail />} />
         <Route path="/training/:slug" element={<TrainingDetail />} />
         <Route path="/iso-training/:slug" element={<IsoTrainingDetail />} />
         <Route path="/info/:slug" element={<InfoPageDetail />} />
         <Route path="/course/:slug" element={<CourseDetail />} />
+        <Route path="/training" element={<TrainingPortal />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/training-login" element={<TrainingLogin />} />
+        <Route path="/admin-training" element={<AdminTraining />} />
+
       </Routes>
     </>
   );
